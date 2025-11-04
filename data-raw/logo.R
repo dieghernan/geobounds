08dc830bae00faacc18c57df9c891b129a4dbd67
Diego H. <diego.hernangomezherrero@gmail.com>
2025-11-04 20:48:02 +0100

    Update logo

--- data-raw/logo.R
+++ data-raw/logo.R
@@ -1,22 +1,21 @@
 ## code to prepare `logo` dataset goes here
 
 # To replace with package functions when available
+library(tidygeoboundaries)
+
+sri <- get_geobn(
+  country = "Sri Lanka",
+  boundary_type = "ADM3", simplified = TRUE
+)
+
 library(hexSticker)
 library(sysfonts)
 ff <- font_families_google()
 
-font_add()
-df <- data.frame(fam = ff)
 font_add_google("Open Sans", "opensans")
 library(ggplot2)
-p <- ggplot(aes(x = mpg, y = wt), data = mtcars) +
-  geom_point()
-p <- p + theme_void() + theme_transparent()
-outfile <- "./data-raw/logo.png"
-sticker(
-  p,
-  package = "tidygeoboundaries",
-  filename = outfile,
-  p_family = "opensans",
-  p_fontface = "bold"
-)
+
+p <- ggplot(sri) +
+  geom_sf(fill = NA, linewidth = 0.1, color = "#F0B323") +
+  theme_void()
+ggsave("data-raw/map.svg", plot = p)
