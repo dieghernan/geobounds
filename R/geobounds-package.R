58a40dab57de4072e6e26b3084333bf64e7aaa0f
Diego H. <dieghernan@users.noreply.github.com>
2026-06-18 00:12:35 +0200

    Add references

--- R/geobounds-package.R
+++ R/geobounds-package.R
@@ -1,4 +1,8 @@
 #' @keywords internal
+#' @references
+#' Runfola, D. et al. (2020) **geoBoundaries**: A global database of political
+#' administrative boundaries. *PLOS ONE* *15*(4), 1-9.
+#' \doi{10.1371/journal.pone.0231866}.
 "_PACKAGE"
 
 ## usethis namespace: start
